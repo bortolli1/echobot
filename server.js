9f799a3ba85cd4d2ba647b02d896a15e4b1920cf
bortolli1 <bortolli1@hotmail.com>
2017-02-08 14:08:32 -0200

    Update server.js

--- server.js
+++ server.js
@@ -30,6 +30,3 @@ server.get(/.*/, restify.serveStatic({
 server.listen(process.env.port || 3978, function () {
     console.log('%s listening to %s', server.name, server.url); 
 });
-bot.dialog('/', function (session) {
-    session.send("Hello World");
-});
